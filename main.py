cc81506b5b86513d9a3c71ceb0a33bebcb6b615e
JaneMusimbi <janemusimbi@gmail.com>
2022-04-22 09:22:23 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+cars=["audi","kbz","subaru","toyota"]
+for car in cars:
+    if car=="kbz":
+        print(car.upper())
+    else:
+            print(car.title())
+age=19
+if age>=18:
+    print("you are old enough to vote")
+y=7
+z=15
+if y < z:
+    print("y is less than z")
+print("Not sure if y is less than z")
+k=9
+l=25
+if k < l:
+    print("k is less than l")
+
+
+
+
